9f96d58f2641077148dad421758706cb023bf67f
andrewbackdev <andrey.demchenko.dev@gmail.com>
2023-08-23 16:12:21 +0300

    Improved client for hosting

---
+++ client/src/config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ServerApiUrl: process.env.REACT_APP_SERVER_API_URL,
+  ServerGraphQlUri: process.env.REACT_APP_SERVER_GRAPHQL_URI,
+  ServerGraphQlWsUrl: process.env.REACT_APP_SERVER_GRAPHQL_WS_URL,
+}
